75484ef65d55a13d397b4a2b4b56753d5b1d3d84
CeriCam <cherycamberly2005@gmail.com>
2025-11-12 18:30:23 +0700

    push

---
+++ script.js
@@ -0,0 +1,19 @@
+function gotoHome(){
+    window.location.href = "index.html";
+}
+
+function gotoNews(){
+    window.location.href = "news.html";
+}
+
+function gotoGallery(){
+    window.location.href = "gallery.html";
+}
+
+function gotoEvent(){
+    window.location.href = "event.html";
+}
+
+function gotoSubscription(){
+    window.location.href = "subscription.html";
+}
